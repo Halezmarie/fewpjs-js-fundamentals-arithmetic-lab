cac28c034971f4fab6b78d16c603eb6ffeb5b608
halezmarie <teacherhaleymarie@gmail.com>
2021-08-23 00:58:17 -0500

    commit tree 81d125ec91d2a60fa4b547087b7699225d5cc85b

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+// "math is fun"
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // 62
+
+const random = Math.floor(Math.random() * 10) + 1;
+// note: floor() function returns the largest integer less than or equal to a given number
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4; // 4
+
+const max = Math.max(10, 20);
